71ab2be27eb2e5b771e0b73731437d4dd40f08f7
zejwant <zejwant@gmail.com>
2025-08-14 18:14:34 +0530

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -1,9 +1,11 @@
 from nip_query import nl_to_sql
-from query_processor import execute_query
-from result_formatter import summarize_dataframe, generate_chart, export_dataframe
 from config import get_settings
 from nip_query.sql_validator_helper import validate_sql
 from query_processor import execute_query
+from nip_query.result_formatter import summarize_dataframe, generate_chart, export_dataframe
+from query_processor import execute_query
+
+
 
 import logging
 
